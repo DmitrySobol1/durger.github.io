a96d7c90f2ededd60a02918793f175d8dd3c1752
DmitrySobol1 <166175811+DmitrySobol1@users.noreply.github.com>
2024-04-06 21:02:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,14 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 let p1 = document.createElement("p");
-var userId = window.Telegram.WebApp.initDataUnsafe.user.id;
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+// разкоментить при деплое
+// var userId = window.Telegram.WebApp.initDataUnsafe.user.id;
 
-p1.innerText = 'id = '+ userId;
+// p.innerText = `${tg.initDataUnsafe.user.first_name}
+// ${tg.initDataUnsafe.user.last_name}`;
+
+// p1.innerText = 'id = '+ userId;
 
 usercard.appendChild(p);
 usercard.appendChild(p1);
@@ -102,10 +104,43 @@ send.addEventListener("click", function(){
 	
 });
 
+sendpost.addEventListener("click", function(){
+	
+    // fetch('https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage?chat_id=1856600462&text=from site');
+	
+const url = 'https://app.leadteh.ru/api/v1/attachTagToContact';
+const data = {
+  api_token: 'YaDoJgaIdZaz6qNG6LGFKVVF5ZaEyoZOI6wOhkIVfuMqIWCT4AEDHnGkwnQv',  
+  contact_id: 21813310,
+  name: 'JS'
+};
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  body: JSON.stringify(data)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Ошибка HTTP: ' + response.status);
+  }
+  return response.json();
+})
+.then(responseData => {
+  console.log('Ответ сервера:', responseData);
+})
+.catch(error => {
+  console.error('Ошибка:', error);
+});
+
+
+});
+
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
 

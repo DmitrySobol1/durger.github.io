302d3b812625912a6b861d1f182c7dc2986357d8
DmitrySobol1 <166175811+DmitrySobol1@users.noreply.github.com>
2024-04-06 23:35:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -97,28 +97,32 @@ btn6.addEventListener("click", function(){
 	}
 });
 
-send.addEventListener("click", function(){
-	fetch(`https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage?chat_id=${userId}&text=yo2`);
-;
 
-	
-});
 
 sendpost.addEventListener("click", function(){
 	
-    // fetch('https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage?chat_id=1856600462&text=from site');
-	
-const url = 'https://app.leadteh.ru/api/v1/attachTagToContact';
-const data = {
-  api_token: 'YaDoJgaIdZaz6qNG6LGFKVVF5ZaEyoZOI6wOhkIVfuMqIWCT4AEDHnGkwnQv',  
-  contact_id: 21813310,
-  name: 'JS'
+    //  fetch('https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage?chat_id=1856600462&text=from site');
+const url = 'https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage';
+var data = {
+  chat_id: '1856600462',
+  text: 'go2',
+  reply_markup: {
+    inline_keyboard: [
+      [
+        {
+          text: "but",
+          url: "https://t.me/learningLT_bot/academyx"
+        }
+      ]
+    ]
+  }
 };
+
 
 fetch(url, {
   method: 'POST',
   headers: {
-    'X-Requested-With': 'XMLHttpRequest'
+    'Content-Type': 'application/json'
   },
   body: JSON.stringify(data)
 })
@@ -130,10 +134,11 @@ fetch(url, {
 })
 .then(responseData => {
   console.log('Ответ сервера:', responseData);
+  console.log(responseData.result.date)
 })
 .catch(error => {
   console.error('Ошибка:', error);
-});
+})
 
 
 });
@@ -144,3 +149,154 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
+
+// send.addEventListener("click", function(){
+
+// fetch('https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/getUpdates')
+//   .then(response => {
+//     // Обработка ответа
+//     if (!response.ok) {
+//       throw new Error('Ошибка HTTP: ' + response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Обработка данных
+//     console.log(data);
+//     var length=data.result.length;
+//     var objectnum=length-1;
+//     var textfromuser=data.result[objectnum].business_message.from.id;
+//     var message_id=data.result[objectnum].business_message.message_id;
+    
+    
+
+//     // send msg to user
+//     const url = 'https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage';
+//     var data = {
+//       business_connection_id: 'Bv-jTk0TWEjzCQAA9Se7DlHB4_c',
+//       chat_id: textfromuser,
+//       text: `ответ из скрипта ${objectnum}`,
+//       reply_parameters : {message_id:message_id}
+      
+//     };
+    
+    
+//     fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Ошибка HTTP: ' + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(responseData => {
+//       console.log('Ответ сервера:', responseData);
+      
+//     })
+//     .catch(error => {
+//       console.error('Ошибка:', error);
+//     })
+        
+//   })
+//   .catch(error => {
+//     // Обработка ошибок
+//     console.error('Ошибка:', error);
+//   });
+	
+// });
+
+var i = 59;
+
+// Запрос каждые 5 сек. Чтобы запустить скрипт, нужно разкоментить строку ниже и в переменную выше занести число последних изменений
+setInterval(function(){
+    
+    fetch('https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/getUpdates')
+      .then(response => {
+        // Обработка ответа
+        if (!response.ok) {
+          throw new Error('Ошибка HTTP: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Обработка данных
+        console.log(data);
+        var length=data.result.length;
+
+if (length > i) {
+      i=i+1;
+
+
+
+
+        var objectnum=length-1;
+        var userid=data.result[objectnum].business_message.from.id;
+        var message_id=data.result[objectnum].business_message.message_id;
+        var usertext=data.result[objectnum].business_message.text;
+        
+
+        var min = 1;
+        var max = 5;
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+       if (randomNumber==1) {
+            text = 'Бот за меня выдает случайные пословицы: Тише едешь — дальше будешь.';
+       } else if (randomNumber==2) {
+            text = 'Бот за меня выдает случайные пословицы: Всякому овощу свое время.';
+       } else if (randomNumber==3) {
+        text = 'Бот за меня выдает случайные пословицы: Не откладывай на завтра то, что можно сделать сегодня.';
+       } else if (randomNumber==4) {
+            text = 'Бот за меня выдает случайные пословицы: Век прожить — не поле перейти.';
+       } else  {
+        text = 'Бот за меня выдает случайные пословицы: Делу время, потехе час.';
+       };
+
+
+
+        
+    
+        // send msg to user
+        const url = 'https://api.telegram.org/bot5626487008:AAHbeWsSvZ1imNZnbHHEWB56IgyT86SlPh8/sendMessage';
+        var data = {
+          business_connection_id: 'Bv-jTk0TWEjzCQAA9Se7DlHB4_c',
+          chat_id: userid,
+          text: `${text} ${objectnum}`,
+          reply_parameters : {message_id:message_id}
+          
+        };
+        
+        
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Ошибка HTTP: ' + response.status);
+          }
+          return response.json();
+        })
+        .then(responseData => {
+          console.log('Ответ сервера:', responseData);
+          
+        })
+        .catch(error => {
+          console.error('Ошибка:', error);
+        })
+            
+      }})
+      .catch(error => {
+        // Обработка ошибок
+        console.error('Ошибка:', error);
+      });
+}      
+    , 5000);
